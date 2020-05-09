5ebae82a078dd016b367b74f9c915dc08ec42786
samia11 <moatasemsalama954@yahoo.com>
2020-05-09 03:22:11 +0200

    edit movie route

--- src/routes/movie.js
+++ src/routes/movie.js
@@ -3,13 +3,10 @@ const router = new express.Router()
 const Movie = require('../models/movie')
 
 router.post('/movie', async (req, res) => {
-    console.log('enter')
+    req.body.title = req.body.title.toLowerCase().trim()
     const movie = await new Movie(req.body)
-    console.log('new movie')
     try{
-        console.log('before save')
         await movie.save()
-        console.log('after save')
         res.status(201).send(movie)
     }
     catch(e){
@@ -18,5 +15,30 @@ router.post('/movie', async (req, res) => {
     
 })
 
-
+router.get('/movies/all', async (req, res) => {
+    try{
+        const movies = await Movie.find( {}, null, 
+            {
+             limit:parseInt(req.query.limit),
+            skip:parseInt(req.query.skip)
+        } )
+        res.status(200).send(movies)    
+    }
+    catch (e){
+        res.status(400).send(e)
+    }
+    
+})
+ 
+router.get('/movies/:title', async (req, res) => {
+    
+        try{
+            const search = req.params.title.match(/extraction/)
+            const movie =  await Movie.find ({title:search})
+            res.send(movie)       
+        }
+        catch(e){
+            res.status(400).send(e)  
+        }
+})
 module.exports = router
